76e58719e6afa18a0f69a63f70eed379fbdc0dde
Jhonatan Nobre <jh_nbs@outlook.com>
2021-01-27 23:17:09 -0400

    criando quiz aula 3

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 import Head from "next/head";
+import { useRouter } from 'next/router';
+
 import db from "../db.json";
 import Widget from "../src/components/Widget";
 import QuizLogo from "../src/components/QuizLogo";
 import QuizBackground from "../src/components/QuizBackground";
 import Footer from "../src/components/Footer";
 import GitHubCorner from "../src/components/GitHubCorner";
-import { useRouter } from 'next/router';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer'
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -14,17 +18,6 @@ import { useRouter } from 'next/router';
 //   background-size: cover;
 //   background-position: center;
 // `;
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {
   const router = useRouter();
@@ -63,16 +56,15 @@ export default function Home() {
               router.push(`/quiz?name=${name}`)
               console.log('Fazendo uma submissão por meio do react');
             }}>
-              <input 
-              onChange={function (infosDoEvento) {
-                console.log(infosDoEvento.target.value)
-                //name = infosDoEvento.target.value;
-                setname(infosDoEvento.target.value);
-              }}
-              placeholder="Diz ai seu nome" />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar {name}
-              </button>
+              <Input 
+              name="nomeDoUsuário"
+              onChange={(infosDoEvento) => setname(infosDoEvento.target.value)}
+              placeholder="Diz ai seu nome" 
+              value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar como ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
