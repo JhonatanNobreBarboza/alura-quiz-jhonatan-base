33bc921416b5fd10ce7039a96a6d8dd91d9a1a6f
Jhonatan Nobre <jh_nbs@outlook.com>
2021-01-31 23:23:24 -0400

    aula 05 Páginas dinâmicas, animações e Lottie

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
-import styled from "styled-components";
-import Head from "next/head";
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 
-import db from "../db.json";
-import Widget from "../src/components/Widget";
-import QuizLogo from "../src/components/QuizLogo";
-import QuizBackground from "../src/components/QuizBackground";
-import Footer from "../src/components/Footer";
-import GitHubCorner from "../src/components/GitHubCorner";
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Link from '../src/components/Link';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
-import QuizContainer from '../src/components/QuizContainer'
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -19,64 +21,110 @@ import QuizContainer from '../src/components/QuizContainer'
 //   background-position: center;
 // `;
 
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 export default function Home() {
   const router = useRouter();
-  const [name, setname] = React.useState('');
-  console.log('retorno do useState', name, setname);
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>
+          AluraQuiz -
+          {db.title}
+        </title>
+      </Head>
       <QuizContainer>
-        <Head>
-          <meta
-            name="title"
-            property="og:title"
-            content="ímersão Alura Quiz - Jhonatan Nobre"
-          />
-          <meta
-            name="image"
-            property="og:image"
-            content="https://github.com/JhonatanNobreBarboza/alura-quiz-jhonatan-base/blob/master/fundoImersao.PNG?raw=true"
-          />
-          <meta
-            name="description"
-            property="og:description"
-            content="Projeto a partir da semana de Imersão React Next.JS - Alura"
-          />
-          <meta name="author" content="Jhonatan Nobre" />
-          <title>AluraQuiz - Jhonatan Nobre</title>
-        </Head>
         <QuizLogo />
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
+            <p>{db.description}</p>
             <form onSubmit={function (infosDoEvento) {
               infosDoEvento.preventDefault();
-              router.push(`/quiz?name=${name}`)
+              router.push(`/quiz?name=${name}`);
               console.log('Fazendo uma submissão por meio do react');
-            }}>
-              <Input 
-              name="nomeDoUsuário"
-              onChange={(infosDoEvento) => setname(infosDoEvento.target.value)}
-              placeholder="Diz ai seu nome" 
-              value={name}
+            }}
+            >
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Diz ai seu nome"
+                value={name}
               />
               <Button type="submit" disabled={name.length === 0}>
-                {`Jogar como ${name}`}
+                {`Jogar ${name}`}
               </Button>
             </form>
           </Widget.Content>
         </Widget>
 
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Content>
             <h1>Quizes da Galera</h1>
 
-            <p>lorem ipsum dolor sit amet...</p>
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('.vercel.app', '')
+                  .split('.');
+
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
           </Widget.Content>
         </Widget>
-        <Footer />
+        <Footer
+          as={motion.footer}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/JhonatanNobreBarboza" />
     </QuizBackground>
